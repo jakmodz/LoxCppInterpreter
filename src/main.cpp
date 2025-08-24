fc31db54f09fe16fbc48e4625ab1e43f154ddce7
jakmodz <modzeleklm@gmail.com>
2025-08-24 17:35:08 +0200

    small fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
 #include  <iostream>
 #include  <string>
-#include <fstream>
 #include  "lox.h"
-#include "Expr.h"
-#include "Obj.h"
-#include "memory"
-#include  "AstPrinter.h"
-#include "Obj.h"    // Use the safe version above
-#include "Env.h"    // Use the safe version above
+
+
 
 int main(int argc, char *argv[])
 {
